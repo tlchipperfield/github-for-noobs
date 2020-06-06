7e162f92e57b196998d2c485b53e244087524e4d
Tyler Chipperfield <tlchipperfield@me.com>
2020-06-06 01:51:59 -0400

    more edits

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var firstName = 'Tyler';
+/*var firstName = 'Tyler';
 console.log(firstName);
 
 var lastName = 'Chip';
@@ -9,16 +9,82 @@ console.log(fullAge);
 
 // ALWAYS USE MEANINGFUL NAMES!!!!
 
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// SIMPLE RULES
+// NO NUMBERS OR SIMBLYOLS
+// _ $ WORK!
+// no keywords
+
+var _test = 'test';
+var $test = true;
+console.log(_test + ' ' + $test);
+*/
 
 
-// TYPES OF VARS
-// Number: Floating point numbers, for decimals and intergers
-// String: Sequence of characters, used for text
-// Boolean: Logical data type that can only be true or false
-// Undefined: Data typ eof a variable that does not have a value yet
-// Null: Also means 'non-existent'
+/* TYPES OF VARS
+Number: Floating point numbers, for decimals and intergers
+ String: Sequence of characters, used for text
+ Boolean: Logical data type that can only be true or false
+ Undefined: Data typ eof a variable that does not have a value yet
+/* Null: Also means 'non-existent'
 
 // JavasScript has dynamic typing: data types are automatically assigned to variables
+
+
+// START 9 COMMENTS SINGLE LINE
+
+
+/****************
+ * Variable mutation and type coercion
+ */
+
+ var firstName = 'Tyler';
+ var age = 30;
+// Type coercion
+ console.log(firstName + ' is ' + age + ' years old!'); //This is actually one big string
+
+ var job, isMarried;
+ job = 'unknown';
+ isMarried = false;
+
+console.log(firstName 'is a ' + age + ' years old' + job + '. Is he married?' + isMarried);
+
+// Variable mutation
+age = 'twent eight';
+job = 'driver';
+
+alert(firstName 'is a ' + age + ' years old' + job + '. Is he married?' + isMarried);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
