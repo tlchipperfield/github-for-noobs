c2b0511e46fcf8b489f5c6b0f87ae97d28053be2
Tyler Chipperfield <tlchipperfield@me.com>
2020-06-06 01:40:16 -0400

    Updated

---
+++ script.js
@@ -0,0 +1,24 @@
+var firstName = 'Tyler';
+console.log(firstName);
+
+var lastName = 'Chip';
+var age = 30;
+
+var fullAge = true;
+console.log(fullAge);
+
+// ALWAYS USE MEANINGFUL NAMES!!!!
+
+
+
+// TYPES OF VARS
+// Number: Floating point numbers, for decimals and intergers
+// String: Sequence of characters, used for text
+// Boolean: Logical data type that can only be true or false
+// Undefined: Data typ eof a variable that does not have a value yet
+// Null: Also means 'non-existent'
+
+// JavasScript has dynamic typing: data types are automatically assigned to variables
+
+
+
