de1a3cd3900884785236df77d2a27a8c43d14713
Tyler Chipperfield <tlchipperfield@me.com>
2020-06-07 19:10:59 -0400

    start challange 2

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ console.log(typeof x);
 /********
  * Operator precedence
  */
-
+/*
 var now = 2018;
 var yearTyler = 1990;
 var fullAge = 18;
@@ -122,11 +122,12 @@ console.log(average);
 var x, y;
 x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
 console.log(x, y);
-
+*/
 // 2 + 4 + 5 Left to Right
 // Assigment '=' Right to Left
 
 // More operators
+/*
 x *= 2;
 console.log(x);
 x += 10;
@@ -135,6 +136,148 @@ x++;
 console.log(x);
 x += 3;
 console.log(x);
+*/
+
+/** 
+ * If / else statements
+ 
+
+ var firstName = 'John';
+ var civilStatus = 'single';
+
+ if (civilStatus === 'married'){
+     console.log(firstName + ' is married');
+ } else {
+     console.log(firstName + ' is not married');
+ }
+
+ var isMarried = true;
+ if (isMarried){
+    console.log(firstName + ' is married');
+} else {
+    console.log(firstName + ' is not married');
+}
+*/
+/*
+var firstName = 'Tyler';
+var age = 16;
+
+if (age < 13) {
+    console.log(firstName + ' is a boy.');
+} else if (age >= 13 && age <= 17) {
+    console.log(firstName + ' is a teenager.');
+} else if (age >=18 && age <= 29) {
+    console.log(firstName + ' is a young man.');
+} else {
+    console.log(firstName + ' is a man');
+}
+*/
+
+/*****
+ * The Ternary Operator and Switch Statements
+ */
+
+ var firstName = 'Tyler';
+ var age = 12;
+
+ age >= 18 ? console.log(firstName + ' drinks beer.')
+ : console.log(firstName + ' drinks juice.');
+
+ var drink = age >= 18 ? 'beer' : 'juice';
+ console.log(drink);
+
+/* if (age >= 18) {
+     var drink = 'beer'
+ } else {
+     var drink = 'juice;'
+ }*/
+/*
+ //switch statement
+ var job = 'cop';
+switch(job) {
+    case 'construction':
+        console.log(firstName + ' is a construction worker.');
+        break;
+    case 'driver' :
+        console.log(firstName + ' is a driver.');
+        break;
+    case 'designer':
+        console.log(firstName + ' is a designer');
+        break;
+    default:
+        console.log(firstName + ' is lazy.');
+}
+
+
+var firstName = 'Tyler';
+var age = 21;
+
+switch(true) {
+    case age < 13:
+        console.log(firstName + ' is a boy.');
+        break;
+    case age >= 13 && age <= 19:
+        console.log(firstName + ' is a teenager.');
+        break;
+    case age >= 20 && age <= 29:
+        console.log(firstName + ' is a young man.');
+        break;
+    default:
+        console.log(firstName + ' is a man.');
+}
+*/
+
+/********
+ * Truthy and Falsy values and equality operators
+ */
+
+ // falsy values: undefined, null, 0, '', NaN
+ // truthy values: NOT falsy values
+
+ /*
+ var height;
+ height = 23;
+
+ if (height || height === 0) {
+     console.log('Variable is defined');
+ } else {
+     console.log('Variable has NOT been defined');
+ }
+
+// Equality operators
+if (height == '23') {
+    console.log('The == operator does type coercion!');
+}
+*/
+
+/****
+ * Coding Challenge 2
+ */
+
+ var teamJohn = [89, 120, 103], teamMike = [116, 94, 123], teamMary = [97, 134, 105];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
