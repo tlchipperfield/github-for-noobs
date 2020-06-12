7ebbfdd3de4933069f5a91e722a961b8b7a4678e
Tyler Chipperfield <tlchipperfield@me.com>
2020-06-12 09:03:24 -0400

    End Lecture #83

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -31,14 +31,18 @@ var budgetController = (function() {
     // Budget Controller Module Returns
     return {
         addItem: function(type, des, val) {
-            var newItem;
+            var newItem, ID;
 
             // create new ID
-            ID = data.allItems[type][data.allItems[type].length -1].id + 1;
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length -1].id + 1;
+            } else {
+                ID = 0;
+            }
 
             // Create new item based on inc or exp type
             if (type === 'exp') {
-                new Expense(ID, des, val);
+                newItem = new Expense(ID, des, val);
             } else if (type === 'inc') {
                 newItem = new Income(ID, des, val);
             }
@@ -48,9 +52,12 @@ var budgetController = (function() {
 
             //return the new element
             return newItem;
+        },
 
+        testing: function() {
+            console.log(data);
         }
-    }
+    };
 
 
 })();
@@ -67,7 +74,9 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list',
     };
 
     // UI Controller Module returns
@@ -79,6 +88,29 @@ var UIController = (function() {
                 value: document.querySelector(DOMstrings.inputValue).value
             };
         },
+
+        addListItem: function(obj, type) {
+            var html, newHtml, element;
+
+            // Create HTML string with PLaceholder text
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type === 'exp') {
+                element = DOMstrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+
+            // Replace the lpaceholder text with some real data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+            
+        },
+
 
         // Allows public pass DOMstrings
         getDOMstrings: function() {
@@ -109,10 +141,15 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     // Add the item to either Income or Expense objects
     var ctrlAddItem = function(){
+
+        var input, newItem;
+
         // 1. Get field input data
         var input = UICtrl.getinput();
         // 2. Add the item to the budget controller
+        var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. Add the item to the UI
+        UICtrl.addListItem(newItem, input.type);
         // 4. Calculate the budget
         // 5. Display budget on UI
     }
